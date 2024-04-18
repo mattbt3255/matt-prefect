bc5d1caec3a58a4ff66e7e8301d5398d153c5f30
Matt Brown <mattbt3255@gmail.com>
2024-04-18 15:54:46 -0500

    Updating dbt flow

--- dbt_flow.py
+++ dbt_flow.py
@@ -6,7 +6,7 @@ def trigger_dbt_flow() -> str:
     result = DbtCoreOperation(
         commands=["pwd", "dbt debug", "dbt run"],
         project_dir="/Users/matt.brown/Documents/matt-dbt",
-        profiles_dir="PROFILES-DIRECTORY-PLACEHOLDER"
+        profiles_path="/Users/matt.brown/.dbt/profiles.yml"
     ).run()
     return result
 

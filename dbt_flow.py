c4adbb591e8ed9d231d477717650af6bbf2e2fd0
Matt Brown <mattbt3255@gmail.com>
2024-04-18 16:15:20 -0500

    Adding dbt_flow

--- dbt_flow.py
+++ dbt_flow.py
@@ -4,10 +4,16 @@ from prefect_dbt.cli.commands import DbtCoreOperation
 @flow
 def trigger_dbt_flow() -> str:
     result = DbtCoreOperation(
-        commands=["pwd", "dbt debug", "dbt run"],
+        commands=["dbt run --models +marts.core"],
         project_dir="/Users/matt.brown/Documents/matt-dbt",
         profiles_path="/Users/matt.brown/.dbt/profiles.yml"
     ).run()
     return result
 
-trigger_dbt_flow()
+if __name__ == "__main__":
+    trigger_dbt_flow.serve(
+        name="dbt-deployment",
+        cron="0 0 * * *",
+        tags=["testing", "tutorial"],
+        description="Runs the training dbt project.",
+    )
